f994d23bd1498557e6caae1b5626800b09dfe350
Olafcio1 <75330538+Olafcio1@users.noreply.github.com>
2022-04-26 07:45:29 +0200

    And more things, again not changed any version numbers
    Add some comments and fix save_backup function in .bashrc_virtualenv.

--- copy.sh
+++ copy.sh
@@ -7,27 +7,32 @@ copy(){
     else
         nf="$2"
     fi
-    echo " `tput setaf 10`$1   `tput setaf 8`COPYING...`tput sgr0`"
+    echo " `tput setaf 8`COPYING...`tput setaf 10`   `tput bold`$nf`tput sgr0`"
     cp $1 $HOME/Documents/PRVim/$nf
 }
 copymod(){
+    if [[ "$2" == "" ]]; then
+        nf="$1"
+    else
+        nf="$2"
+    fi
     f="$1"
     cp $f .copied_$f
     vim .copied_$f
-    copy .copied_$f $f
+    copy .copied_$f $nf
     rm .copied_$f
 }
 ignoremsg(){
-    echo " `tput setaf 3`$1   `tput setaf 8`IGNORED.`tput sgr0`"
+    echo " `tput setaf 8`IGNORED.  `tput setaf 3`   `tput bold`$1`tput sgr0`"
 }
-echo "`tput setaf 3``tput bold`WARN:`tput setaf 3` If you see any messages with 'IGNORED' text but you want to copy ignored files, run program again with --full parameter or --other-modify parameter."
+echo " `tput setaf 3``tput bold`WARN:`tput setaf 3` If you see any messages with 'IGNORED' text but you want to copy ignored files, run program again with --full parameter or --other-modify parameter.`tput sgr0`"
 if [[ "$1" == "--full" ]]; then
     copy .bashrc
 elif [[ "$1" == "--other-modify" ]]; then
     copymod .bashrc
 else
     ignoremsg .bashrc
-    copy .bashrc .bashrc_virtualenv
+    copymod .bashrc .bashrc_virtualenv
 fi
 copy .vimrc
 for file in `ls`; do
